74078c20c53799a03531153683f32187fe1da39f
uniArchitect <hancho93@gmail.com>
2022-08-04 21:46:53 -0400

    PROGRESS: Add task.js with Task class

---
+++ src/task.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor(name, dueDate, description) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.priority = '';
+        this.description = '';
+        this.checklist = [];
+    }
+}
