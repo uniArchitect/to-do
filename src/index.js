475cb619c821845a9da3072c11fccb07d693609c
uniArchitect <hancho93@gmail.com>
2022-08-04 23:01:41 -0400

    FEAT: Close and open task form

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import Task from './task.js';
 // GLOBAL SCOPE
 const TASK_FORM_DIV = document.querySelector('.task-form-background')
 const CREATE_TASK_BTN = document.querySelector('.task-create')
+const CLOSE_TASK_BTN = document.querySelector('.close-task-form')
 
 // Event - Open Task Form when click on 'CREATE A TASK'
 CREATE_TASK_BTN.addEventListener('click', (e) => {
     TASK_FORM_DIV.style.display = 'block';
 })
 
+CLOSE_TASK_BTN.addEventListener('click', (e) => {
+    TASK_FORM_DIV.style.display = 'none';
+})
