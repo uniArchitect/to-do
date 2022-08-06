ba6d735b50e71b45ff798b1f25643cf8e3c4462c
uniArchitect <hancho93@gmail.com>
2022-08-05 23:13:05 -0400

    BUG: addSideBarLink does not work, but addCategoryContainer function does

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import './styles.css';
 import Task from './task.js';
 import UI from './addTask.js';
-import Category from './addCategory';
+import Category, { addCategoryContainer, addSideBarLink } from './addCategory';
 
 // GLOBAL SCOPE
 const TASK_FORM_DIV = document.querySelector('.task-form-background')
 const CREATE_TASK_BTN = document.querySelector('.task-create')
 const CLOSE_TASK_BTN = document.querySelector('.close-task-form')
+const NEW_CATEGORY_BTN = document.querySelector('.create-category')
 
 // Event - Open Task Form when click on 'CREATE A TASK'
 CREATE_TASK_BTN.addEventListener('click', () => {
     TASK_FORM_DIV.style.display = 'block';
 })
 
-// Event - Open Category Form when click on 'NEW CATEGORY'
+// Event - Append Category to ul when clicked
+NEW_CATEGORY_BTN.addEventListener('click', () => {
+    const catName = 'New Category!'
+    const category = new Category(catName);
 
+    Category.addCategoryContainer();
+    Category.addSideBarLink();
+})
 
 // Event - Close Task Form when click 'Close'
 CLOSE_TASK_BTN.addEventListener('click', () => {
