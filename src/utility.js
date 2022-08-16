54edb90d66e571f2658b61c4ea6523724a6e14e9
uniArchitect <hancho93@gmail.com>
2022-08-15 23:03:50 -0400

    PROGRESS: update utility.js

--- src/utility.js
+++ src/utility.js
@@ -1,1 +1,12 @@
 // Utility functions used throughout To-Do project
+
+// category.js
+
+// task.js
+
+export default spreadElements = () => {
+    const otherElements = document.querySelector('.main-UI').children;
+    const siblingsArray = [...otherElements];
+
+    return siblingsArray
+}
