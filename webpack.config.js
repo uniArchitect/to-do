e4c88feb15466b1bb11c238b14305d1eee982884
uniArchitect <hancho93@gmail.com>
2022-08-26 23:49:30 -0400

    UPDATE: webpack.config.js up to date

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       // styles css
-    //   {
-    //     test: /\.css$/i,
-    //     use: ['style-loader', 'css-loader'],
-    //   },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
       // images
-    //   {
-    //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
-    //     type: 'asset/resource',
-    //   },
+      //   {
+      //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      //     type: 'asset/resource',
+      //   },
       // fonts
-    //   {
-    //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
-    //     type: 'asset/resource',
-    //   },
-    ]
-  }
+      //   {
+      //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      //     type: 'asset/resource',
+      //   },
+    ],
+  },
 };
